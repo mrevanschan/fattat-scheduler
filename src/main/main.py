1af0026debcc4ed8707955baac5b2f93b456b560
evanschan <mrevanschan@gmail.com>
2022-10-23 23:43:37 +0800

    start polling

--- src/main/main.py
+++ src/main/main.py
@@ -2,19 +2,21 @@ from rocketry import Rocketry
 from rocketry.conds import every
 import logging
 from logging import config
+from services.post_scrapper import __get_recent_posts
 import os
 
 
-config.fileConfig(fname='log.ini')
+config.fileConfig(fname='../../log.ini')
 logging.getLogger('rocketry.task').propagate = False
 logging.info("Application Started")
 app = Rocketry()
 
 
-@app.task(every("5 seconds"))
+@app.task(every("60 seconds"))
 def poll_new_post():
     logging.info("SCHEDULE JOB: POLLING FB")
-    logging.info(os.environ.get('DATABASE_URL'))
+    for post in __get_recent_posts():
+        logging.info(post)
 
 
 if __name__ == "__main__":

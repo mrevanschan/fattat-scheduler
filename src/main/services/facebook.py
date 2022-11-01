d9c5bc59c7848119d2a9099946acd76456dbc7a0
evanschan <mrevanschan@gmail.com>
2022-11-01 21:54:40 +0800

    start polling

--- src/main/services/facebook.py
+++ src/main/services/facebook.py
@@ -108,6 +108,7 @@ class Facebook:
                     pass
             return post_texts
         except Exception as ex:
+            logging.info(browser.page_source)
             logging.info(f"exception: {ex}")
         finally:
             browser.close()

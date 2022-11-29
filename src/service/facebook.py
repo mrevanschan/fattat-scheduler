825fb99ee0e47994252090ff2a82a96f9732ff1c
evanschan <mrevanschan@gmail.com>
2022-11-30 00:59:05 +0800

    add credentials

--- src/service/facebook.py
+++ src/service/facebook.py
@@ -1,11 +1,11 @@
 import logging
-# import os
+import os
 # import time
 from facebook_scraper import get_posts
 
 def scrape():
     try:
-        posts = get_posts("fatchaitat", pages=1)
+        posts = get_posts("fatchaitat", pages=1,credentials=(os.environ['FB_USER'], os.environ['FB_PASSCODE']))
         logging.info(f"scaped: {len(posts)}")
         for post in posts:
             logging.info( post["text"])
